d1963899b3a97894e5cb97ffafdd8a197d6e8d9d
nem <nmccready@gmail.com>
2013-01-29 16:24:21 -0500

    - handle moving around an application from app to app.last

--- bash/stage.sh
+++ bash/stage.sh
@@ -1,11 +1,32 @@
 #get correct arguments
-jenkinsJobName=$1
-destinationAddress=$2
-stageLocation=$3
-#can grap any zip in dist as there is only every one in there
-packageLocation=scp' '/var/lib/jenkins/jobs/$jenkinsJobName/workspace/dist/*.zip 
-#add url
+jenkinsJobName=$1 #example proj1-stage
+destinationAddress=$2 #
+stageHome=$3 #example ~/stage/
+applicationName=$4 #example proj1
 
-destination=$destinationAddress:$stageLocation
-cmd=$packageLocation' '$destination
+applicationNameNew = $applicationName'.new'
+
+#create the package location by replacing the jenkinsJobName variable with the actual jobName
+packageLocation=/var/lib/jenkins/jobs/$jenkinsJobName/workspace/dist/*.zip
+
+#stagePath will combine the stageHome location to the subdirectory structure we want
+#ie: if stageHome = '~/stage/'' and applicationName = 'proj1'
+#then stagePath = ~/stage/proj1/ 
+stagePath=$stageHome''$applicationName'/'
+#append appNew, #then stagePath = ~/stage/proj1/proj1.new
+stagePathWithNewApplication = $stagePath''$applicationNameNew
+
+#destination will combine address/hostname to a full stagePath, this will fulfill the scp destination
+#ie: 127.0.0.1:~/stage/proj1/proj1New
+destination=$destinationAddress:$stagePathWithNewApplication
+#pack full scp line
+cmd=scp''$packageLocation' '$destination
+#execute scp
 $cmd
+
+#ssh into puppet machine
+ssh $destinationAddress
+cd $stagePath
+mv $applicationName'.last' $applicationName'.last.bak'
+mv $applicationName $applicationName'.last
+mv $applicationNameNew $applicationName
