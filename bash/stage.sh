b92f065b8d8263bd6bf2a07d336beb11bd254140
nem <nmccready@gmail.com>
2013-01-29 17:56:47 -0500

    test

--- bash/stage.sh
+++ bash/stage.sh
@@ -33,6 +33,7 @@ ssh $destinationAddress applicationName=$applicationName 'bash -s' <<'ENDSSH'
   rollBackApp=$applicationName'.last.bak'
   # commands to run on remote host
   cd $stagePath
+  pwd
   #if [ -b "$device0" ]
   #then
   #	echo "$device0 is a block device.
