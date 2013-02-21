00f82589f4d261e1d171efbe063ec2e5a1c46c1a
Nick McCready <nmccready@gmail.com>
2013-02-21 18:21:33 -0500

    CD-5 fixing scripts

--- main-scripts/runInSSH/stageRemote/topargs.sh
+++ main-scripts/runInSSH/stageRemote/topargs.sh
@@ -1,6 +1,11 @@
 applicationName=${1?missing application name}
+
 stagePath=${2?missing stage path}
+
 extension=${3?missing extension}
+
 destinationAddress=${4?missing destination address}
+
 extractCmd=${5?missing extraction command like "unzip"}
-applicationPortNumber=${5?missing port number for application hosting}
+
+applicationPortNumber=${6?missing port number for application hosting}
