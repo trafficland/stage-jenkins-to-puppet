36a7017c818d6dd1f5c1766217c1ef2d8fed9bc8
Nick McCready <nmccready@gmail.com>
2013-02-28 21:03:52 -0500

    CD-9 - path set up for stage home had a missing ending slash

--- puppet-substantiator/app/controllers/ScriptController.scala
+++ puppet-substantiator/app/controllers/ScriptController.scala
@@ -25,7 +25,7 @@ abstract class ScriptController extends Controller {
     }
   }
 
-  val puppetServerStageHome = getOptionOrDefault(play.api.Play.configuration.getString("puppet.stage.home"), "~/stage")
+  val puppetServerStageHome = getOptionOrDefault(play.api.Play.configuration.getString("puppet.stage.home"), "~/stage/")
   val extension = getOptionOrDefault(play.api.Play.configuration.getString("puppet.stage.extension"), "~/.zip")
   val puppetHostNameOrAddress = getOptionOrDefault(play.api.Play.configuration.getString("puppet.hostName"), "127.0.0.1")
   val extractCommand = getOptionOrDefault(play.api.Play.configuration.getString("puppet.stage.extractCommand"), "unzip")
@@ -55,7 +55,7 @@ abstract class ScriptController extends Controller {
                 |applicationPortNumber=${6?missing port number for application hosting}
               """
               actors().getActor(scriptorName) ! new Script(scriptPathAndName.replaceFirst(".", new File(".").getCanonicalPath()),
-                Seq(appName, puppetServerStageHome + "/" + appName, extension, puppetHostNameOrAddress, extractCommand, appPortNumber.toString))
+                Seq(appName, puppetServerStageHome + appName + "/", extension, puppetHostNameOrAddress, extractCommand, appPortNumber.toString))
               Ok("Execute Rollback script here!")
             case None =>
               InternalServerError("Script not Found!")
